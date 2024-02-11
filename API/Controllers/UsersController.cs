7fb3a21b1f59f353d23bb239fbe06b8c4a73a6e7
Youness <youness.dradeb@gmail.com>
2024-02-11 22:55:10 +0100

    Controller added

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext context)
+        {
+           _dataContext = context;
+        }
+
+
+        [HttpGet]
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _dataContext.Users.ToList();
+            return users;
+        }
+
+
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            var user = _dataContext.Users.FirstOrDefault(x => x.Id == id); 
+            return user;
+        }
+
+    }
+}
