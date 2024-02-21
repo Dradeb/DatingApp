b5bec9852f4d0d536303fcb44dcd7e465c7196ef
DRADEB-EXTERNAL Youness <youness.dradeb@external.thalesgroup.com>
2024-02-21 14:52:19 +0100

    adding client project

---
+++ API/Controllers/UsersController.cs
@@ -0,0 +1,36 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _dataContext;
+
+        public UsersController(DataContext context)
+        {
+           _dataContext = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            var users = await _dataContext.Users.ToListAsync();
+            return users;
+        }
+
+
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            var user = await _dataContext.Users.FirstOrDefaultAsync(x => x.Id == id); 
+            return user;
+        }
+
+    }
+}
---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,11 @@
+﻿namespace API.Entities
+{
+    public class AppUser
+    {
+
+        public int Id { get; set; }
+        public string UserName { get; set; }
+        
+
+    }
+}
